d5ca1d19992975656caf247cd9e072c0660f5120
Alessandro Iepure <39530238+aleiepure@users.noreply.github.com>
2022-08-18 12:15:18 +0200

    Timestamp now supports timezones

--- devtoolbox/widgets/timestamp_utility.py
+++ devtoolbox/widgets/timestamp_utility.py
@@ -18,6 +18,8 @@
 from datetime import datetime
 from gettext import gettext as _
 from gi.repository import Gtk, Adw, Gio
+from pytz import common_timezones
+from zoneinfo import ZoneInfo
 
 
 @Gtk.Template(resource_path="/me/iepure/devtoolbox/ui/timestamp_utility.ui")
@@ -37,9 +39,7 @@ class TimestampUtility(Adw.Bin):
     hours_spinner = Gtk.Template.Child()
     minutes_spinner = Gtk.Template.Child()
     seconds_spinner = Gtk.Template.Child()
-    #timezone_combo = Gtk.Template.Child()
-    #timezone_combo_model = Gtk.Template.Child()
-    #action_row = Gtk.Template.Child()
+    action_row = Gtk.Template.Child()
 
     settings = Gio.Settings(schema_id="me.iepure.devtoolbox")
 
@@ -49,28 +49,15 @@ class TimestampUtility(Adw.Bin):
         # Favorites button icon
         fav_list = self.settings.get_strv("favorites")
         try:
-            fav_list.index("timestamp")  # check if present, throws error if not
+            # check if present, throws error if not
+            fav_list.index("timestamp")
             self.starred_btn.set_icon_name("starred-symbolic")
         except ValueError:
             self.starred_btn.set_icon_name("non-starred-symbolic")
 
         # Populate timezone list
-        #model = Gtk.StringList()
-        # model = Gtk.ListStore(GObject.GType(Timezone))
-        # for tz in pytz.common_timezones:
-        #     timezone = Timezone(_(tz))
-        #     #print(timezone)
-        #     model.append(timezone)
-        #     #model.append(_(tz))
-        # dropdown = Gtk.DropDown()
-        # dropdown.set_model(model)
-        # #dropdown.set_enable_search(True)
-        # #dropdown.set_expression(Gtk.PropertyExpression.new(Gtk.Label, None, "label"))
-        # expression = Gtk.PropertyExpression(GObject.GType(Timezone), None, "name")
-        # dropdown.set_expression(expression)
-        # dropdown.set_enable_search(True)
-        # #print(dropdown.get_expression())
-        # self.action_row.add_suffix(dropdown)
+        self.timezone_dropdown = self.create_drop_down()
+        self.action_row.add_suffix(self.timezone_dropdown)
 
         # Connect button signals
         self.clear_btn.connect("clicked", self.on_clear_clicked)
@@ -79,20 +66,52 @@ class TimestampUtility(Adw.Bin):
         self.clearDate_btn.connect("clicked", self.on_clearDate_clicked)
         self.starred_btn.connect("clicked", self.on_star_clicked)
         self.settings.connect("changed", self.on_settings_changed)
-        self.timestamp_spinner.connect("value-changed", self.on_timestamp_changed)
+        self.timestamp_spinner.connect(
+            "value-changed", self.on_timestamp_changed)
         self.year_spinner.connect("value-changed", self.on_date_changed)
         self.month_spinner.connect("value-changed", self.on_date_changed)
         self.day_spinner.connect("value-changed", self.on_date_changed)
-        self.hours_spinner.connect("value-changed", self.on_date_changed)        
-        self.minutes_spinner.connect("value-changed", self.on_date_changed)        
-        self.seconds_spinner.connect("value-changed", self.on_date_changed)        
-        
+        self.hours_spinner.connect("value-changed", self.on_date_changed)
+        self.minutes_spinner.connect("value-changed", self.on_date_changed)
+        self.seconds_spinner.connect("value-changed", self.on_date_changed)
+        self.timezone_dropdown.connect("notify::selected-item", self.on_changed_timezone)
+
+    def on_changed_timezone(self, param_spec, data):
+        self.on_timestamp_changed(None)
+
+    def create_drop_down(self):
+        string_list_items = "\n".ljust(11).join(
+            [f"<item>{time_zone}</item>" for time_zone in common_timezones]
+        )
+
+        drop_down_ui_string = f"""<interface>
+  <object class="GtkDropDown" id="drop-down">
+    <property name="model">
+      <object class="GtkStringList">
+        <items>
+          {string_list_items}
+        </items>
+      </object>
+    </property>
+    <property name="enable-search">true</property>
+    <property name="expression">
+      <lookup type="GtkStringObject" name="string"></lookup>
+    </property>
+    <property name="valign">center</property>
+  </object>
+</interface>"""
+
+        builder = Gtk.Builder.new_from_string(drop_down_ui_string, -1)
+        drop_down = builder.get_object("drop-down")
+
+        return drop_down
 
     def on_settings_changed(self, key, data):
         # Favorites button icon
         fav_list = self.settings.get_strv("favorites")
         try:
-            fav_list.index("timestamp") # check if present, throws error if not
+            # check if present, throws error if not
+            fav_list.index("timestamp")
             self.starred_btn.set_icon_name("starred-symbolic")
         except ValueError:
             self.starred_btn.set_icon_name("non-starred-symbolic")
@@ -100,7 +119,8 @@ class TimestampUtility(Adw.Bin):
     def on_star_clicked(self, data):
         fav_list = self.settings.get_strv("favorites")
         try:
-            fav_list.index("timestamp")  # check if present, throws error if not
+            # check if present, throws error if not
+            fav_list.index("timestamp")
             self.starred_btn.set_icon_name("non-starred-symbolic")
             fav_list.remove("timestamp")
             self.settings.set_strv("favorites", fav_list)
@@ -112,7 +132,9 @@ class TimestampUtility(Adw.Bin):
             self.toast.add_toast(Adw.Toast(title=_("Added to favorites!")))
 
     def on_timestamp_changed(self, data):
-        time = datetime.fromtimestamp(self.timestamp_spinner.get_value())
+        tz = self.timezone_dropdown.get_selected_item().get_string()
+        time = datetime.fromtimestamp(
+            self.timestamp_spinner.get_value(), tz=ZoneInfo(tz))
         self.year_spinner.set_value(time.year)
         self.month_spinner.set_value(time.month)
         self.day_spinner.set_value(time.day)
@@ -124,7 +146,8 @@ class TimestampUtility(Adw.Bin):
         self.timestamp_spinner.set_value(0)
 
     def on_now_clicked(self, data):
-        self.timestamp_spinner.set_value(datetime.now().timestamp())
+        tz = self.timezone_dropdown.get_selected_item().get_string()
+        self.timestamp_spinner.set_value(datetime.now(tz=ZoneInfo(tz)).timestamp())
 
     def on_date_changed(self, data):
         year = int(self.year_spinner.get_value())
@@ -133,7 +156,10 @@ class TimestampUtility(Adw.Bin):
         hour = int(self.hours_spinner.get_value())
         minute = int(self.minutes_spinner.get_value())
         second = int(self.seconds_spinner.get_value())
-        self.timestamp_spinner.set_value(datetime(year, month, day, hour, minute, second).timestamp())
+        tz = self.timezone_dropdown.get_selected_item().get_string()
+        timestamp = datetime(year, month, day, hour=hour, minute=minute,
+                             second=second, tzinfo=ZoneInfo(tz)).timestamp()
+        self.timestamp_spinner.set_value(timestamp)
 
     def on_clearDate_clicked(self, data):
         self.year_spinner.set_value(1970)
@@ -144,7 +170,8 @@ class TimestampUtility(Adw.Bin):
         self.seconds_spinner.set_value(0)
 
     def on_nowDate_clicked(self, data):
-        current_time = datetime.now()
+        tz = self.timezone_dropdown.get_selected_item().get_string()
+        current_time = datetime.now(tz=ZoneInfo(tz))
         self.year_spinner.set_value(current_time.year)
         self.month_spinner.set_value(current_time.month)
         self.day_spinner.set_value(current_time.day)
