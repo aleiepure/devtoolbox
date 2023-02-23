53ce6b3d391db731c5da32a5842cc8e797ff53a7
Alessandro Iepure <39530238+aleiepure@users.noreply.github.com>
2023-02-23 16:39:10 +0100

    Text inspector: changed case buttons to dropdown

--- src/views/text_inspector.py
+++ src/views/text_inspector.py
@@ -15,20 +15,7 @@ class TextInspectorView(Adw.Bin):
     # Template elements
     _toast = Gtk.Template.Child()
     _title = Gtk.Template.Child()
-    _original_case_btn = Gtk.Template.Child()
-    _sentence_case_btn = Gtk.Template.Child()
-    _lower_case_btn = Gtk.Template.Child()
-    _upper_case_btn = Gtk.Template.Child()
-    _title_case_btn = Gtk.Template.Child()
-    _camel_case_btn = Gtk.Template.Child()
-    _pascal_case_btn = Gtk.Template.Child()
-    _snake_case_btn = Gtk.Template.Child()
-    _constant_case_btn = Gtk.Template.Child()
-    _kebab_case_btn = Gtk.Template.Child()
-    _cobol_case_btn = Gtk.Template.Child()
-    _train_case_btn = Gtk.Template.Child()
-    _alternating_case_btn = Gtk.Template.Child()
-    _reverse_alternating_case_btn = Gtk.Template.Child()
+    _case_dropdown = Gtk.Template.Child()
     _line_lbl = Gtk.Template.Child()
     _column_lbl = Gtk.Template.Child()
     _chars_lbl = Gtk.Template.Child()
@@ -42,20 +29,7 @@ class TextInspectorView(Adw.Bin):
         super().__init__()
 
         # Signals
-        self._original_case_btn.connect("clicked", self._on_original_case_btn_clicked)
-        self._sentence_case_btn.connect("clicked", self._on_sentence_case_btn_clicked)
-        self._lower_case_btn.connect("clicked", self._on_lower_case_btn_clicked)
-        self._upper_case_btn.connect("clicked", self._on_upper_case_btn_clicked)
-        self._title_case_btn.connect("clicked", self._on_title_case_btn_clicked)
-        self._camel_case_btn.connect("clicked", self._on_camel_case_btn_clicked)
-        self._pascal_case_btn.connect("clicked", self._on_pascal_case_btn_clicked)
-        self._snake_case_btn.connect("clicked", self._on_snake_case_btn_clicked)
-        self._constant_case_btn.connect("clicked", self._on_constant_case_btn_clicked)
-        self._kebab_case_btn.connect("clicked", self._on_kebab_case_btn_clicked)
-        self._cobol_case_btn.connect("clicked", self._on_cobol_case_btn_clicked)
-        self._train_case_btn.connect("clicked", self._on_train_case_btn_clicked)
-        self._alternating_case_btn.connect("clicked", self._on_alternating_case_btn_clicked)
-        self._reverse_alternating_case_btn.connect("clicked", self._on_reverse_alternating_case_btn_clicked)
+        self._case_changed_handler = self._case_dropdown.connect("notify::selected", self._on_case_changed)
         self._textarea.connect("cursor-moved", self._on_text_cursor_moved)
         self._textarea.connect("view-cleared", self._on_view_cleared)
         self._text_changed_handler = self._textarea.connect("text-changed", self._on_text_changed)
@@ -64,73 +38,46 @@ class TextInspectorView(Adw.Bin):
         self._textarea.set_spinner_spin(False)
         self._service.get_cancellable().cancel()
 
-    def _on_original_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_text(self._original_text)
+    def _on_case_changed(self, source_widget:GObject.Object, pspec:GObject.ParamSpec):
 
-    def _on_sentence_case_btn_clicked(self, user_data:GObject.GPointer):
+        # Stop previous tasks
+        self._service.get_cancellable().cancel()
+        self._textarea.set_spinner_spin(False)
+
+        # Setup task
         self._textarea.set_spinner_spin(True)
         self._service.set_text(self._original_text)
-        self._service.to_sentence_case_async(self, self._on_async_done)
 
-    def _on_lower_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_lower_case_async(self, self._on_async_done)
-
-    def _on_upper_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_upper_case_async(self, self._on_async_done)
-
-    def _on_title_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_title_case_async(self, self._on_async_done)
-
-    def _on_camel_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_camel_case_async(self, self._on_async_done)
-
-    def _on_pascal_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_pascal_case_async(self, self._on_async_done)
-
-    def _on_snake_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_snake_case_async(self, self._on_async_done)
-
-    def _on_constant_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_constant_case_async(self, self._on_async_done)
-
-    def _on_kebab_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_kebab_case_async(self, self._on_async_done)
-
-    def _on_cobol_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_cobol_case_async(self, self._on_async_done)
-
-    def _on_train_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_train_case_async(self, self._on_async_done)
-
-    def _on_alternating_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_alternating_case_async(self, self._on_async_done)
-
-    def _on_reverse_alternating_case_btn_clicked(self, user_data:GObject.GPointer):
-        self._textarea.set_spinner_spin(True)
-        self._service.set_text(self._original_text)
-        self._service.to_reverse_alternating_case_async(self, self._on_async_done)
+        # Call task
+        match self._case_dropdown.get_selected():
+            case 0: # Original
+                self._textarea.set_text(self._original_text)
+            case 1: # Sentence case
+                self._service.to_sentence_case_async(self, self._on_async_done)
+            case 2: # lower case
+                self._service.to_lower_case_async(self, self._on_async_done)
+            case 3: # UPPER CASE
+                self._service.to_upper_case_async(self, self._on_async_done)
+            case 4: # Title Case
+                self._service.to_title_case_async(self, self._on_async_done)
+            case 5: # camelCase
+                self._service.to_camel_case_async(self, self._on_async_done)
+            case 6: # PascalCase
+                self._service.to_pascal_case_async(self, self._on_async_done)
+            case 7: # snake_case
+                self._service.to_snake_case_async(self, self._on_async_done)
+            case 8: # CONSTANT_CASE
+                self._service.to_constant_case_async(self, self._on_async_done)
+            case 9: # kebab-case
+                self._service.to_kebab_case_async(self, self._on_async_done)
+            case 10: # COBOL-CASE
+                self._service.to_cobol_case_async(self, self._on_async_done)
+            case 11: # Train-Case
+                self._service.to_train_case_async(self, self._on_async_done)
+            case 12: # aLtErNaTiNg CaSe
+                self._service.to_alternating_case_async(self, self._on_async_done)
+            case 13: # ReVeRsE aLtErNaTiNg CaSe
+                self._service.to_reverse_alternating_case_async(self, self._on_async_done)
 
     def _on_text_cursor_moved(self, source_widget:GObject.Object):
         insert_mark = self._textarea.get_buffer().get_insert()
@@ -140,15 +87,22 @@ class TextInspectorView(Adw.Bin):
 
     def _on_async_done(self, source_widget:GObject.Object, result:Gio.AsyncResult, user_data:GObject.GPointer):
         outcome = self._service.async_finish(result, self)
+
         self._textarea.handler_block(self._text_changed_handler)
         self._textarea.set_text(outcome)
         self._textarea.handler_unblock(self._text_changed_handler)
+
         self._calculate_stats()
         self._textarea.set_spinner_spin(False)
 
     def _on_text_changed(self, source_widget:GObject.Object):
-        self._original_case_btn.set_active(True)
-        text = self._textarea.get_text()
+        self._service.get_cancellable().cancel()
+
+        self._case_dropdown.handler_block(self._case_changed_handler)
+        self._case_dropdown.set_selected(0)
+        self._case_dropdown.handler_unblock(self._case_changed_handler)
+
+       text = self._textarea.get_text()
         self._original_text = text
         self._calculate_stats()
 
