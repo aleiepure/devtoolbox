45954de89fd8224cc6df15461ed7edf0bd6d12ed
aleiepure <39530238+aleiepure@users.noreply.github.com>
2024-10-01 18:02:11 +0200

    Timestamp: added more date formats

--- src/views/timestamp.py
+++ src/views/timestamp.py
@@ -21,6 +21,12 @@ class TimestampView(Adw.Bin):
     _timestamp_spin_area = Gtk.Template.Child()
     _date_area = Gtk.Template.Child()
     _iso_date = Gtk.Template.Child()
+    _rfc2822_date = Gtk.Template.Child()
+    _short_date = Gtk.Template.Child()
+    _short_time = Gtk.Template.Child()
+    _long_date = Gtk.Template.Child()
+    _long_time = Gtk.Template.Child()
+    _full_long_date = Gtk.Template.Child()
 
     def __init__(self):
         super().__init__()
@@ -37,17 +43,28 @@ class TimestampView(Adw.Bin):
             tz = "Europe/Kyiv"
 
         self._timezone_dropdown.set_selected(all_timezones.index(tz))
-        time = datetime.now(tz = ZoneInfo(tz))
+        time = datetime.now(tz=ZoneInfo(tz))
         self._timestamp_spin_area.set_value(time.timestamp())
-        self._date_area.set_date(time.year, time.month, time.day, time.hour, time.minute, time.second)
+        self._date_area.set_date(
+            time.year, time.month, time.day, time.hour, time.minute, time.second)
         self._iso_date.set_text(time.isoformat("T"))
+        self._rfc2822_date.set_text(time.strftime("%a, %d %b %Y %H:%M:%S %z"))
+        self._short_date.set_text(time.strftime("%x"))
+        self._short_time.set_text(time.strftime("%X"))
+        self._long_date.set_text(time.strftime("%B %d %Y"))
+        self._long_time.set_text(time.strftime("%I:%M:%S %p"))
+        self._full_long_date.set_text(
+            time.strftime("%A, %B %d %Y %I:%M:%S %p %Z"))
 
         # Signals
-        self._timestamp_spin_area.connect("action-clicked", self._on_timestamp_now_clicked)
-        self._timestamp_spin_area.connect("value-changed", self._on_timestamp_changed)
+        self._timestamp_spin_area.connect(
+            "action-clicked", self._on_timestamp_now_clicked)
+        self._timestamp_spin_area.connect(
+            "value-changed", self._on_timestamp_changed)
         self._date_area.connect("now-clicked", self._on_date_now_clicked)
         self._date_area.connect("value-changed", self._on_date_changed)
-        self._timezone_dropdown.connect("notify::selected-item", self._on_changed_timezone)
+        self._timezone_dropdown.connect(
+            "notify::selected-item", self._on_changed_timezone)
 
     def _create_dropdown(self):
         string_list_items = "\n".ljust(11).join(
@@ -79,25 +96,49 @@ class TimestampView(Adw.Bin):
         self._on_timestamp_changed(None)
         self._on_date_changed(None)
 
-    def _on_timestamp_now_clicked(self, source_widget:GObject.Object):
+    def _on_timestamp_now_clicked(self, source_widget: GObject.Object):
         tz = self._timezone_dropdown.get_selected_item().get_string()
         date_time = datetime.now(tz=ZoneInfo(tz))
         self._timestamp_spin_area.set_value(int(date_time.timestamp()))
         self._iso_date.set_text(date_time.isoformat("T"))
 
-    def _on_date_now_clicked(self, source_widget:GObject.Object):
+    def _on_date_now_clicked(self, source_widget: GObject.Object):
         tz = self._timezone_dropdown.get_selected_item().get_string()
         time = datetime.now(tz=ZoneInfo(tz))
-        self._date_area.set_date(time.year, time.month, time.day, time.hour, time.minute, time.second)
+        self._date_area.set_date(
+            time.year, time.month, time.day, time.hour, time.minute, time.second)
 
-    def _on_timestamp_changed(self, source_widget:GObject.Object):
+    def _on_timestamp_changed(self, source_widget: GObject.Object):
         tz = self._timezone_dropdown.get_selected_item().get_string()
-        time = datetime.fromtimestamp(self._timestamp_spin_area.get_value(), tz=ZoneInfo(tz))
-        self._date_area.set_date(time.year, time.month, time.day, time.hour, time.minute, time.second)
+        time = datetime.fromtimestamp(
+            self._timestamp_spin_area.get_value(), tz=ZoneInfo(tz))
+        self._date_area.set_date(
+            time.year, time.month, time.day, time.hour, time.minute, time.second)
         self._iso_date.set_text(time.isoformat("T"))
+        self._rfc2822_date.set_text(time.strftime("%a, %d %b %Y %H:%M:%S %z"))
+        self._short_date.set_text(time.strftime("%x"))
+        self._short_time.set_text(time.strftime("%X"))
+        self._long_date.set_text(time.strftime("%B %d %Y"))
+        self._long_time.set_text(time.strftime("%I:%M:%S %p"))
+        self._full_long_date.set_text(
+            time.strftime("%A, %B %d %Y %I:%M:%S %p %Z"))
 
-    def _on_date_changed(self, source_widget:GObject.Object):
+    def _on_date_changed(self, source_widget: GObject.Object):
         tz = self._timezone_dropdown.get_selected_item().get_string()
         year, month, day, hours, minutes, seconds = self._date_area.get_date()
-        self._timestamp_spin_area.set_value(int(datetime(year, month, day, hour=hours, minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).timestamp()))
-        self._iso_date.set_text(datetime(year, month, day, hour=hours, minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).isoformat("T"))
+        self._timestamp_spin_area.set_value(int(datetime(
+            year, month, day, hour=hours, minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).timestamp()))
+        self._iso_date.set_text(datetime(year, month, day, hour=hours,
+                                minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).isoformat("T"))
+        self._rfc2822_date.set_text(datetime(year, month, day, hour=hours, minute=minutes,
+                                    second=seconds, tzinfo=ZoneInfo(tz)).strftime("%a, %d %b %Y %H:%M:%S %z"))
+        self._short_date.set_text(datetime(year, month, day, hour=hours,
+                                  minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).strftime("%x"))
+        self._short_time.set_text(datetime(year, month, day, hour=hours,
+                                  minute=minutes, second=seconds, tzinfo=ZoneInfo(tz)).strftime("%X"))
+        self._long_date.set_text(datetime(year, month, day, hour=hours, minute=minutes,
+                                 second=seconds, tzinfo=ZoneInfo(tz)).strftime("%B %d %Y"))
+        self._long_time.set_text(datetime(year, month, day, hour=hours, minute=minutes,
+                                 second=seconds, tzinfo=ZoneInfo(tz)).strftime("%I:%M:%S %p"))
+        self._full_long_date.set_text(datetime(year, month, day, hour=hours, minute=minutes,
+                                      second=seconds, tzinfo=ZoneInfo(tz)).strftime("%A, %B %d %Y %I:%M:%S %p %Z"))
