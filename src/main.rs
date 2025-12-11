607c6c6061046454f33cbb7e15bb00e036ff66ec
aleiepure <39530238+aleiepure@users.noreply.github.com>
2025-12-11 18:38:10 +0100

    Init with GTK Rust Template

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod application;
+#[rustfmt::skip]
+mod config;
+mod window;
+
+use gettextrs::{gettext, LocaleCategory};
+use gtk::{gio, glib};
+
+use self::application::ExampleApplication;
+use self::config::{GETTEXT_PACKAGE, LOCALEDIR, RESOURCES_FILE};
+
+fn main() {
+    // Initialize logger
+    pretty_env_logger::init();
+
+    // Prepare i18n
+    gettextrs::setlocale(LocaleCategory::LcAll, "");
+    gettextrs::bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR).expect("Unable to bind the text domain");
+    gettextrs::textdomain(GETTEXT_PACKAGE).expect("Unable to switch to the text domain");
+
+    glib::set_application_name(&gettext("Devtoolbox"));
+
+    gtk::init().expect("Unable to start GTK4");
+
+    let res = gio::Resource::load(RESOURCES_FILE).expect("Could not load gresource file");
+    gio::resources_register(&res);
+
+    let app = ExampleApplication::new();
+    app.run();
+}
